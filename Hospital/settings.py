07db7c6d4988fb8bcc28ff69145bc72285bba3f6
gurulakshmikanth <150226696+gurulakshmikanth@users.noreply.github.com>
2026-04-29 14:58:01 +0530

    Update settings.py

--- Hospital/settings.py
+++ Hospital/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Hospital.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hospitaldb',
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+        'HOST': 'postgresql',
+        'PORT': '5432',
     }
 }
 
